dd54d43fe12f7364c02d80ab5d379594ac328537
aagnone <aagnone@dminc.com>
2022-05-07 10:11:40 -0400

    Created skeleton hook, basic file structure, initialization and a tad bit of procrastination as I think

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import './index.css'
 
-function App() {
+const App = () => {
+  const [solution, setSolution] = useState(null)
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(res => res.json())
+      .then(json => {
+        const randomSolution = json[Math.floor(Math.random() * json.length)]
+        setSolution(randomSolution.word)
+      })
+    return
+  }, [setSolution])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Random word {solution && <h1>{solution}</h1>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/hooks/useWordle.js
@@ -0,0 +1,5 @@
+const useWordle = (solution) => {
+    
+}
+
+export default useWordle
